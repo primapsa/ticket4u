d27a10908c3c3e0e5b392bb80ad9002a29cdeb86
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-07 20:42:15 +0300

    feat: add get method for concerts

--- ticket4uApp/t4uApp/views.py
+++ ticket4uApp/t4uApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from rest_framework import status
 
-# Create your views here.
+from .models import Concerts
+from .serializers import *
+
+
+@api_view(['GET', 'POST'])
+def concert_list(request):
+    if request.method == 'GET':
+        data = Concerts.objects.all()
+        serializer = ConcertsSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+
