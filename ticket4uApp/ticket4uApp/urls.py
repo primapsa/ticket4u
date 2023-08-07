cf44ceb2fe16425054acc9a71bbac757080ff86a
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-07 20:42:43 +0300

    feat: add routing

--- ticket4uApp/ticket4uApp/urls.py
+++ ticket4uApp/ticket4uApp/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from t4uApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/concerts/$', views.concert_list),
 ]
