632be4c6878a1179c716713de5178e43abf34918
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-07 20:40:52 +0300

    feat: add  concerts model

--- ticket4uApp/t4uApp/models.py
+++ ticket4uApp/t4uApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Concerts(models.Model):
+    title = models.CharField(max_length=100)
+    singer = models.CharField(max_length=100)
+    ticketsID = models.AutoField
+    date = models.DateTimeField
+    placeId = models.IntegerField
+    typeId = models.IntegerField
+    singerVoiceId = models.IntegerField
+    concertName = models.CharField(max_length=100, null=True)
+    composer = models.CharField(max_length=100, null=True)
+    wayHint = models.CharField(max_length=100, null=True)
+    headliner = models.CharField(max_length=100, null=True)
+    censor = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.title
