7952815224d4b190e301b17f3b19060a4cc65002
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-08 21:23:37 +0300

    feat: add serializers

--- ticket4uApp/t4uApp/serializers.py
+++ ticket4uApp/t4uApp/serializers.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
-from .models import Concerts
+from .models import Concerts, Place, ConcertType, SingerVoice
 
 
 class ConcertsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Concerts
-        fields = ('id', 'title', 'singer', 'ticketsId', 'date', 'placeId', 'typeId', 'singerVoiceId', 'concertName',
-                  'composer', 'wayHint', 'headliner', 'censor')
+        fields = '__all__'
+
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = ('title', 'latitude', 'longitude')
+
+
+class ConcertsExtendedSerializer(serializers.ModelSerializer):
+    place = PlaceSerializer()
+    price = serializers.FloatField()
+    tickets = serializers.IntegerField()
+
+    class Meta:
+        model = Concerts
+        fields = ('title', 'singer', 'placeId', 'typeId', 'place', 'price', 'tickets')
+
+
+class ConcertTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConcertType
+        fields = '__all__'
+
+class SingerVoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SingerVoice
+        fields = '__all__'
+
+class CartSerializer(seria)
