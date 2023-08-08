73bd8b88b9ad96f248dc25d7a34b47fca1ab1875
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-08 17:41:34 +0300

    feat: add db models

--- ticket4uApp/t4uApp/models.py
+++ ticket4uApp/t4uApp/models.py
@@ -1,4 +1,3 @@
-
 from django.utils import timezone
 from django.db import models
 
@@ -6,10 +5,9 @@ from django.db import models
 class Concerts(models.Model):
     title = models.CharField(max_length=100)
     singer = models.CharField(max_length=100)
-    ticketsId = models.IntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
-    placeId = models.IntegerField(default=0)
-    typeId = models.IntegerField(default=0)
+    placeId = models.IntegerField()
+    typeId = models.IntegerField()
     singerVoiceId = models.IntegerField(default=0)
     concertName = models.CharField(max_length=100, null=True)
     composer = models.CharField(max_length=100, null=True)
@@ -19,3 +17,45 @@ class Concerts(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tickets(models.Model):
+    concertId = models.IntegerField()
+    userId = models.IntegerField(default=0)
+    statusId = models.IntegerField(default=0)
+    promocodeId = models.IntegerField(default=0)
+    price = models.FloatField()
+    finalPrice = models.FloatField()
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+
+
+class ConcertType(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class SingerVoice(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class TicketStatus(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Cart(models.Model):
+    userId = models.IntegerField()
+    ticketId = models.IntegerField()
+
+
+class Promocode(models.Model):
+    title = models.CharField(max_length=100)
+    concertId = models.IntegerField()
+    discount = models.IntegerField()
+
+
+
+
