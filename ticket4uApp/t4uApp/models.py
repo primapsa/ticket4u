a883925e47c5d486f35c29c517d852eb00af4d41
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-09 22:30:06 +0300

    refactor: foreign key

--- ticket4uApp/t4uApp/models.py
+++ ticket4uApp/t4uApp/models.py
@@ -2,12 +2,16 @@ from django.utils import timezone
 from django.db import models
 
 
+class ConcertType(models.Model):
+    title = models.CharField(max_length=100)
+
+
 class Concerts(models.Model):
     title = models.CharField(max_length=100)
     singer = models.CharField(max_length=100)
     date = models.DateTimeField(default=timezone.now)
     placeId = models.IntegerField(default=0)
-    typeId = models.IntegerField()
+    typeId = models.ForeignKey(ConcertType, default=0, on_delete=models.SET_DEFAULT, related_name='types')
     singerVoiceId = models.IntegerField(default=0)
     concertName = models.CharField(max_length=100, null=True)
     composer = models.CharField(max_length=100, null=True)
@@ -19,11 +23,22 @@ class Concerts(models.Model):
         return self.title
 
 
-class Tickets(models.Model):
+class TicketStatus(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Promocode(models.Model):
+    title = models.CharField(max_length=100)
     concertId = models.IntegerField()
+    discount = models.IntegerField()
+
+
+class Tickets(models.Model):
+    concertId = models.ForeignKey(Concerts, on_delete=models.CASCADE, null=False)
     userId = models.IntegerField(default=0)
-    statusId = models.IntegerField(default=1)
-    promocodeId = models.IntegerField(default=0)
+    statusId = models.ForeignKey(TicketStatus, default=1, on_delete=models.SET_DEFAULT)
+    promocodeId = models.ForeignKey(Promocode, default=1, on_delete=models.SET_DEFAULT)
+
     price = models.FloatField()
     finalPrice = models.FloatField()
 
@@ -34,28 +49,10 @@ class Place(models.Model):
     longitude = models.CharField(max_length=100)
 
 
-class ConcertType(models.Model):
-    title = models.CharField(max_length=100)
-
-
 class SingerVoice(models.Model):
-    title = models.CharField(max_length=100)
-
-
-class TicketStatus(models.Model):
     title = models.CharField(max_length=100)
 
 
 class Cart(models.Model):
     userId = models.IntegerField()
     ticketId = models.IntegerField()
-
-
-class Promocode(models.Model):
-    title = models.CharField(max_length=100)
-    concertId = models.IntegerField()
-    discount = models.IntegerField()
-
-
-
-
