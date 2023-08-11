becb7f9b1fb91f0e9dd538ffa829954b9214f875
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-11 19:25:22 +0300

    feat: add serailizers fields

--- ticket4uApp/t4uApp/serializers.py
+++ ticket4uApp/t4uApp/serializers.py
@@ -1,11 +1,38 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Concerts, Place, ConcertType, SingerVoice, Cart, Promocode
+
+
+class ConcertTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConcertType
+        fields = '__all__'
 
 
 class ConcertsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Concerts
         fields = '__all__'
+
+
+class ConcertsSerializerEx(serializers.ModelSerializer):
+    type = serializers.CharField(source='typeId__title')
+    latitude = serializers.CharField(source='placeId__latitude')
+    longitude = serializers.CharField(source='placeId__longitude')
+    voice = serializers.CharField(source='singerVoiceId__title')
+
+    class Meta:
+        model = Concerts
+        fields = ('id', 'title', 'singer', 'concertName', 'composer',
+                  'wayHint', 'headliner', 'censor', 'date', 'latitude', 'longitude', 'type', 'voice')
+
+
+class ConcertsSerializerOutput(serializers.Serializer):
+    data = ConcertsSerializerEx()
+    results = serializers.IntegerField()
+
+    # class Meta:
+    #     fields = ('data', 'results')
 
 
 class PlaceSerializer(serializers.ModelSerializer):
@@ -24,12 +51,6 @@ class ConcertsExtendedSerializer(serializers.ModelSerializer):
         fields = ('title', 'singer', 'placeId', 'typeId', 'place', 'price', 'tickets')
 
 
-class ConcertTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ConcertType
-        fields = '__all__'
-
-
 class SingerVoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = SingerVoice
@@ -41,7 +62,14 @@ class CartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = '__all__'
 
+
 class PromocodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promocode
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
