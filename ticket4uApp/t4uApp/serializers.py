59616f96e96ca3d5956ec2781f18adf468663458
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-17 22:15:06 +0300

    feat: extend serialzers

--- ticket4uApp/t4uApp/serializers.py
+++ ticket4uApp/t4uApp/serializers.py
@@ -4,9 +4,12 @@ from .models import Concerts, Place, ConcertType, SingerVoice, Cart, Promocode
 
 
 class ConcertTypeSerializer(serializers.ModelSerializer):
+    value = serializers.CharField(source='id')
+    label = serializers.CharField(source='title')
+
     class Meta:
         model = ConcertType
-        fields = '__all__'
+        fields = ('value', 'label')
 
 
 class ConcertsSerializer(serializers.ModelSerializer):
@@ -17,28 +20,21 @@ class ConcertsSerializer(serializers.ModelSerializer):
 
 class ConcertsSerializerEx(serializers.ModelSerializer):
     type = serializers.CharField(source='typeId__title')
+    address = serializers.CharField(source='placeId__address')
     latitude = serializers.CharField(source='placeId__latitude')
     longitude = serializers.CharField(source='placeId__longitude')
     voice = serializers.CharField(source='singerVoiceId__title')
 
     class Meta:
         model = Concerts
-        fields = ('id', 'title', 'singer', 'concertName', 'composer',
-                  'wayHint', 'headliner', 'censor', 'date', 'latitude', 'longitude', 'type', 'voice')
-
-
-class ConcertsSerializerOutput(serializers.Serializer):
-    data = ConcertsSerializerEx()
-    results = serializers.IntegerField()
-
-    # class Meta:
-    #     fields = ('data', 'results')
+        fields = ('id', 'title', 'concertName', 'composer',
+                  'wayHint', 'headliner', 'censor', 'date', 'address', 'latitude', 'longitude', 'type', 'voice')
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
-        fields = ('title', 'latitude', 'longitude')
+        fields = ('address', 'latitude', 'longitude')
 
 
 class ConcertsExtendedSerializer(serializers.ModelSerializer):
@@ -48,13 +44,30 @@ class ConcertsExtendedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Concerts
-        fields = ('title', 'singer', 'placeId', 'typeId', 'place', 'price', 'tickets')
+        fields = ('title', 'date', 'typeId', 'singerVoiceId', 'concertName',
+                  'composer', 'wayHint', 'headliner', 'censor', 'place', 'poster', 'price', 'tickets')
+
+
+class ConcertsExtendedSerializer2(serializers.ModelSerializer):
+    price = serializers.FloatField()
+    tickets = serializers.IntegerField()
+    address = serializers.CharField(max_length=100)
+    latitude = serializers.CharField(max_length=100)
+    longitude = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = Concerts
+        fields = ('title', 'date', 'typeId', 'singerVoiceId', 'concertName',
+                  'composer', 'wayHint', 'headliner', 'censor', 'address','latitude','longitude', 'poster', 'price', 'tickets')
 
 
 class SingerVoiceSerializer(serializers.ModelSerializer):
+    value = serializers.CharField(source='id')
+    label = serializers.CharField(source='title')
+
     class Meta:
         model = SingerVoice
-        fields = '__all__'
+        fields = ('value', 'label')
 
 
 class CartSerializer(serializers.ModelSerializer):
