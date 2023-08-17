f3333818f7f6ae17ef2255e35f36f8558f8977c9
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-17 22:16:35 +0300

    feat: add utilite function

--- ticket4uApp/t4uApp/utils.py
+++ ticket4uApp/t4uApp/utils.py
@@ -1,3 +1,6 @@
+import uuid
+
+
 def make_bulk(num, obj):
     bulk = []
     counter = 1
@@ -5,3 +8,7 @@ def make_bulk(num, obj):
         bulk.append(obj)
         counter += 1
     return bulk
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return 'images/{filename}'.format(filename=filename)
