4e65740ddb1509f81330a3f743c5c4a1c6a9de82
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-29 14:51:53 +0300

    feat: add auth view

---
+++ ticket4uApp/t4uApp/auth/view.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializer import RegisterSerializer, UserSerializer, MyTokenObtainPairSerializer
+
+
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user,    context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.  Now perform Login to get your token",
+        })
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
