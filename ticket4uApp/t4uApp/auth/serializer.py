a16666c54de71bf39a0ed4a462333f38e2de46e0
Siarhei Prymakovich <prima.psa@gmail.com>
2023-08-29 14:51:32 +0300

    feat: add auth serializer

---
+++ ticket4uApp/t4uApp/auth/serializer.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email')
+        extra_kwargs = {'password': {'write_only': True}, }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        password=validated_data['password'],
+                                        first_name=validated_data['first_name'],
+                                        last_name=validated_data['last_name'],
+                                        email=validated_data['email'])
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['is_staff'] = user.is_staff
+        token['email'] = user.email
+        return token
