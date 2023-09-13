f9f8c2672a7fda979725ffb5e00b7d0a67fe7d8f
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-13 18:21:33 +0300

    refactor: path

--- ticket4uApp/ticket4uApp/urls.py
+++ ticket4uApp/ticket4uApp/urls.py
@@ -48,4 +48,4 @@ urlpatterns = [
                   path('api/user/me/', views.me),
                   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),                
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] 
