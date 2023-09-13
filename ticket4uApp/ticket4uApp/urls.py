bafc070e7353b6c24b8247f990c16341103769cf
Siarhei Prymakovich <prima.psa@gmail.com>
2023-09-13 17:48:48 +0300

    refactor: static urls

--- ticket4uApp/ticket4uApp/urls.py
+++ ticket4uApp/ticket4uApp/urls.py
@@ -47,5 +47,5 @@ urlpatterns = [
                   path('api/user/register/', RegisterApi.as_view()),
                   path('api/user/me/', views.me),
                   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-                  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),                 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),                
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
