9236abe791a3f548a045b0cbe7cf2b92db60e13f
Siarhei Prymakovich <prima.psa@gmail.com>
2023-10-09 22:37:51 +0300

    refactor: space

--- ticket4uApp/ticket4uApp/urls.py
+++ ticket4uApp/ticket4uApp/urls.py
@@ -7,11 +7,10 @@ from django.conf.urls.static import static
 from t4uApp import views
 from t4uApp.urls import *
 
-
 urlpatterns = [
     path("api/v1/concerts/", include(concerts_url)),
     path("api/v1/type/", views.ConcertTypeList.as_view()),
-    path("api/v1/voice/", views.SingerVoiceList.as_view()),
+    path("api/v1/voice/", views.SingerVoiceList.as_view()), 
     path("api/v1/promocode/", views.PromocodeCardDetail.as_view()),
     path("api/v1/promocodes/", include(promocodes_url)),
     path("api/v1/paypal/", include(paypal_url)),
