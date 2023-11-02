7743c94c7b0c8ff1d8291ecf037c0963ef6f36d2
Siarhei Prymakovich <prima.psa@gmail.com>
2023-11-02 16:04:03 +0300

    feat: throw new serializers

--- ticket4uApp/t4uApp/serializers/concert/__init__.py
+++ ticket4uApp/t4uApp/serializers/concert/__init__.py
@@ -1,1 +1,1 @@
-from .serializer import ConcertTypeSerializer, ConcertsTypePlaceSingerSerializer, ConcertsSerializer, ConcertsSerializerEx, PlaceSerializer, ConcertsExtendedSerializer, ConcertsAddresSerializer, SingerVoiceSerializer
+from .serializer import ConcertTypeSerializer, ConcertsTypePlaceSingerSerializer, ConcertsSerializer, ConcertsSerializerEx, PlaceSerializer, ConcertsExtendedSerializer, ConcertsAddresSerializer, SingerVoiceSerializer, ConcertPartySerializer, ConcertOpenairSerializer,ConcertClassicSerializer,ConcertExtraSerializer,ConcertsUpdateSerializer
